3e7956f70f9bc32158d056ead92aad3ef65b0c97
Olivier Robert <olivierobert@gmail.com>
2020-03-26 10:48:37 +0700

    Handle upload of non-CSV files

--- lib/crawlexir/search/csv.ex
+++ lib/crawlexir/search/csv.ex
@@ -3,32 +3,24 @@ defmodule Crawlexir.Search.Csv do
   @max_keyword_count 100
   @template_header "keyword"
 
-  @doc """
-  Parse keywords list from a CSV file.
-  Parsing relies on the provided template stored in ""/static/csv/template.csv"
-
-  ## Examples
-
-      iex> "./assets/static/csv/template.csv"
-      iex> |> Path.expand(__DIR__)
-      iex> |> Crawlexir.Search.Csv.parse
-      {:ok, ["first_keyword", "second_keyword"]}
-
-  """
   def parse(file) do
-    case parse_with_headers(file) do
-      content_list when length(content_list) > 0 ->
-        keyword_list = Enum.map(content_list, fn {k, v} -> v[@template_header] end)
+    try do
+      case parse_with_headers(file) do
+        [_ | _] = content_list ->
+          keyword_list = Enum.map(content_list, fn item -> item[@template_header] end)
+          {:ok, keyword_list}
 
-        {:ok, keyword_list}
-      [] ->
-        {:error, "Invalid CSV format"}
+        [] ->
+          {:error, "Invalid CSV format"}
+      end
+    rescue
+      _e -> {:error, "File cannot be parsed"}
     end
   end
 
   defp parse_with_headers(file) do
     File.stream!(file)
-    |> CSV.decode(headers: true)
+    |> CSV.decode!(headers: true)
     |> Enum.take(@max_keyword_count)
   end
 end
