a7d5ea429d79366a03158d4eac6afda3bc8886e4
Olivier Robert <olivierobert@gmail.com>
2020-04-01 15:32:58 +0700

    Add status column to keywors to track scraping status

--- lib/crawlexir/search/scraping_status.ex
+++ lib/crawlexir/search/scraping_status.ex
@@ -1,3 +1,3 @@
 import EctoEnum
 
-defenum Crawlexir.Search.ScrapingStatusEnum, pending: 0, in_progress: 1, completed: 2, failed: 3
+defenum(Crawlexir.Search.ScrapingStatusEnum, pending: 0, in_progress: 1, completed: 2, failed: 3)
