5eeb852799e0eb9c95ddbbcce5038bebdf4a34c3
Olivier Robert <olivierobert@gmail.com>
2020-04-15 11:54:23 +0700

    Remove unused module

--- priv/repo/migrations/20200401080357_add_status_to_keyword.exs
+++ priv/repo/migrations/20200401080357_add_status_to_keyword.exs
@@ -1,8 +1,6 @@
 defmodule Crawlexir.Repo.Migrations.AddStatusToKeyword do
   use Ecto.Migration
 
-  alias Crawlexir.Search.Keyword
-
   def change do
     alter table("keywords") do
       add :status, :integer, default: 0
